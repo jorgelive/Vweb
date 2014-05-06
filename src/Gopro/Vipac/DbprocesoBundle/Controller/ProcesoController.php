2eb5dea70d915981fd69560beab0e8f36cb69554
Jorge Luis Gomez <jgomezv@outlook.com>
2014-05-05 20:30:38 -0500

    Agregando calculadora de files

--- src/Gopro/Vipac/DbprocesoBundle/Controller/ProcesoController.php
+++ src/Gopro/Vipac/DbprocesoBundle/Controller/ProcesoController.php
@@ -43,7 +43,7 @@ class ProcesoController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($archivo);
             $em->flush();
-            return $this->redirect($this->generateUrl('gopro_vipac_dbproceso_carga_generico'));
+            return $this->redirect($this->generateUrl('gopro_vipac_dbproceso_proceso_cheque'));
         }
 
         if($archivoEjecutar!==null){
@@ -101,5 +101,99 @@ class ProcesoController extends Controller
         return array('formulario' => $formulario->createView(),'archivosAlmacenados' => $archivosAlmacenados, 'resultados'=>$resultados ,'mensajes' => $mensajes);
     }
 
+    /**
+     * @Route("/proceso/calculadora/{archivoEjecutar}", name="gopro_vipac_dbproceso_proceso_calculadora", defaults={"archivoEjecutar" = null})
+     * @Template()
+     */
+    public function calculadoraAction(Request $request,$archivoEjecutar)
+    {
+        $usuario=$this->get('security.context')->getToken()->getUser();
+
+        $repositorio = $this->getDoctrine()->getRepository('GoproVipacDbprocesoBundle:Archivo');
+        $archivosAlmacenados=$repositorio->findBy(array('usuario' => $usuario, 'operacion' => 'proceso_calculadora'));
+
+        $archivo = new Archivo();
+        $formulario = $this->createFormBuilder($archivo)
+            ->add('nombre')
+            ->add('file')
+            ->getForm();
+
+        $formulario->handleRequest($request);
+
+        if ($formulario->isValid()){
+            $archivo->setUsuario($usuario);
+            $archivo->setOperacion('proceso_calculadora');
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($archivo);
+            $em->flush();
+            return $this->redirect($this->generateUrl('gopro_vipac_dbproceso_proceso_calculadora'));
+        }
+
+        if($archivoEjecutar!==null){
+            $archivoAlmacenado=$repositorio->find($archivoEjecutar);
+        }
+        if(isset($archivoAlmacenado)&&$archivoAlmacenado->getOperacion()=='proceso_calculadora'){
+            $tablaSpecs=array('schema'=>'RESERVAS',"nombre"=>'VVW_FILES_MERCADO');
+
+
+            $columnaspecs[0]=array('nombre'=>'ANO-NUM_FILE','llave'=>'si','tipo'=>'file');
+            $columnaspecs[1]=array('nombre'=>'FECHA','llave'=>'no','tipo'=>'exceldate','proceso'=>'no');
+            $columnaspecs[2]=array('nombre'=>'MONTO','llave'=>'no','proceso'=>'no');
+            $columnaspecs[3]=array('nombre'=>'CENTRO_COSTO','llave'=>'no');
+            $columnaspecs[4]=array('nombre'=>'NUM_PAX','llave'=>'no');
+            $columnaspecs[5]=array('nombre'=>'LLEGADA','llave'=>'no');
+            $columnaspecs[6]=array('nombre'=>'SALIDA','llave'=>'no');
+            $procesoArchivo=$this->get('gopro_dbproceso_comun_archivo');
+            $procesoArchivo->setParametros($archivoAlmacenado->getAbsolutePath(),$tablaSpecs,$columnaspecs);
+            $mensajes=$procesoArchivo->getMensajes();
+            if($procesoArchivo->parseExcel()!==false){
+                $carga=$this->get('gopro_dbproceso_comun_cargador');
+                $carga->setParametros($procesoArchivo->getTablaSpecs(),$procesoArchivo->getColumnaSpecs(),$procesoArchivo->getValores(),$this->container->get('doctrine.dbal.default_connection'));
+                $carga->cargaGenerica();
+                $existente=$carga->getExistente();
+                $fusion=array_replace_recursive($existente,$procesoArchivo->getValoresIndizados());
+
+                $phpExcelObject = $this->get('phpexcel')->createPHPExcelObject();
+
+                $phpExcelObject->getProperties()->setCreator("Viapac")
+                    ->setTitle("Documento Generado")
+                    ->setDescription("Documento generado para descargar");
+                $phpExcelObject->setActiveSheetIndex(0)
+                    ->setCellValue('A1', 'FECHA')
+                    ->setCellValue('B1', 'NOMBREPA')
+                    ->setCellValue('C1', 'ANO')
+                    ->setCellValue('D1', 'FILE')
+                    ->setCellValue('D1', 'LLEGADA')
+                    ->setCellValue('D1', 'SALIDA');
+
+                $phpExcelObject->setActiveSheetIndex(0)->fromArray($fusion, NULL, 'A2');
+                $phpExcelObject->getActiveSheet()->setTitle('Hoja de datos');
+
+                // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+                $phpExcelObject->setActiveSheetIndex(0);
+
+                // create the writer
+                $writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel2007');
+                // create the response
+                $response = $this->get('phpexcel')->createStreamedResponse($writer);
+                // adding headers
+                $response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
+                $response->headers->set('Content-Disposition', 'attachment;filename=archivo.xlsx');
+                $response->headers->set('Pragma', 'public');
+                $response->headers->set('Cache-Control', 'maxage=1');
+
+                return $response;
+
+            }else{
+                $mensajes=array_merge($mensajes,array('El archivo no se puede procesar'));
+            }
+            //$mensajes = $this->get('gopro_dbproceso_comun_cargador')->ejecutar($tablaSpecs,$columnaSpecs,$valores);
+
+        }else{
+            $mensajes[]='El archivo no existe';
+        }
+
+        return array('formulario' => $formulario->createView(),'archivosAlmacenados' => $archivosAlmacenados, 'mensajes' => $mensajes);
+    }
 
 }
