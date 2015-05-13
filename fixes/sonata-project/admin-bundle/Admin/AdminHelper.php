d0cb3613d0f336266ccd2574d3db388ae1c67a17
Jorge Luis Gomez (desktop 1) <jgomezv@outlook.com>
2015-05-13 10:24:44 -0500

    Fixes: AdminBundle: actualizacion del controlador y el helper

--- fixes/sonata-project/admin-bundle/Admin/AdminHelper.php
+++ fixes/sonata-project/admin-bundle/Admin/AdminHelper.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of the Sonata package.
  *
@@ -7,14 +8,29 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Sonata\AdminBundle\Admin;
+
+use Doctrine\Common\Inflector\Inflector;
+use Doctrine\Common\Util\ClassUtils;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormView;
+use Sonata\AdminBundle\Exception\NoValueException;
 use Sonata\AdminBundle\Util\FormViewIterator;
+use Sonata\AdminBundle\Util\FormBuilderIterator;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
 use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class AdminHelper
+ *
+ * @package Sonata\AdminBundle\Admin
+ * @author  Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ */
 class AdminHelper
 {
     protected $pool;
+
     /**
      * @param Pool $pool
      */
@@ -22,6 +38,7 @@ class AdminHelper
     {
         $this->pool = $pool;
     }
+
     /**
      * @param \Symfony\Component\Form\FormView $formView
      * @param string                           $elementId
@@ -35,8 +52,10 @@ class AdminHelper
                 return $formView;
             }
         }
+
         return null;
     }
+
     /**
      * @deprecated
      *
@@ -48,7 +67,13 @@ class AdminHelper
     {
         return $this->pool->getInstance($code);
     }
+
     /**
+     * Note:
+     *   This code is ugly, but there is no better way of doing it.
+     *   For now the append form element action used to add a new row works
+     *   only for direct FieldDescription (not nested one)
+     *
      * @throws \RuntimeException
      *
      * @param \Sonata\AdminBundle\Admin\AdminInterface $admin
@@ -60,50 +85,88 @@ class AdminHelper
     public function appendFormFieldElement(AdminInterface $admin, $subject, $elementId)
     {
         // retrieve the subject
-        $form = $admin->getFormBuilder()->getForm();
+        $formBuilder = $admin->getFormBuilder();
+
+        $form = $formBuilder->getForm();
         $form->setData($subject);
-        $form->submit($admin->getRequest());
+        $form->handleRequest($admin->getRequest());
+
         $elementId = preg_replace('#.(\d+)#', '[$1]', implode('.', explode('_', substr($elementId, strpos($elementId, '_') + 1))));
+
         // append a new instance into the object
         $this->addNewInstance($admin, $elementId);
+
         // return new form with empty row
         $finalForm = $admin->getFormBuilder()->getForm();
         $finalForm->setData($subject);
         $finalForm->setData($form->getData());
+
         return $finalForm;
     }
+
     /**
      * Add a new instance
      *
      * @param AdminInterface $admin
      * @param string         $elementId
      *
-     * @throws \RuntimeException
+     * @throws \Exception
      */
-    protected function addNewInstance(AdminInterface $admin, $elementId)
+    public function addNewInstance(AdminInterface $admin, $elementId)
     {
         $entity = $admin->getSubject();
+
         $propertyAccessor = new PropertyAccessor();
+
         $collection = $propertyAccessor->getValue($entity, $elementId);
-        if ($collection instanceof ArrayCollection) {
+
+        if ($collection instanceof \Doctrine\ORM\ArrayCollection) {
             $entityClassName = $this->entityClassNameFinder($admin, explode('.', preg_replace('#\[\d*?\]#', '', $elementId)));
         } elseif ($collection instanceof \Doctrine\ORM\PersistentCollection) {
             //since doctrine 2.4
             $entityClassName = $collection->getTypeClass()->getName();
         } else {
-            throw new \Exception('unknown collection class');
+            return;
         }
+
+        if (!method_exists($collection, 'add')){
+            return;
+        }
+
         $collection->add(new $entityClassName);
+
         $propertyAccessor->setValue($entity, $elementId, $collection);
     }
+
+
     protected function entityClassNameFinder(AdminInterface $admin, $elements)
     {
         $element = array_shift($elements);
+
+        if(!is_object($element)){
+            return null;
+        }
+
         $associationAdmin = $admin->getFormFieldDescription($element)->getAssociationAdmin();
+
         if (count($elements) == 0) {
             return $associationAdmin->getClass();
         } else {
             return $this->entityClassNameFinder($associationAdmin, $elements);
         }
+    }
+
+    /**
+     * Camelize a string
+     *
+     * @static
+     *
+     * @param string $property
+     *
+     * @return string
+     */
+    public function camelize($property)
+    {
+        return BaseFieldDescription::camelize($property);
     }
 }
